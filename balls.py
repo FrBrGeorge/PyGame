e8aef68c9702531b97da806dd33987fd9b92a8b7
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2015-02-27 15:43:47 +0300

    Game with balls

--- balls.py
+++ balls.py
@@ -2,8 +2,12 @@
 # coding: utf
 
 import pygame
+import random
 
 SIZE = 640, 480
+
+def intn(*arg):
+    return tuple(map(int,arg))
 
 def Init(sz):
     '''Turn PyGame on'''
@@ -13,15 +17,17 @@ def Init(sz):
 
 class GameMode:
     '''Basic game mode class'''
+    def __init__(self):
+        self.background = pygame.Color("black")
 
     def Events(self,event):
         '''Event parser'''
         pass
 
     def Draw(self, screen):
-        pass
+        screen.fill(self.background)
 
-    def Logic(self):
+    def Logic(self, screen):
         '''What to calculate'''
         pass
 
@@ -33,16 +39,100 @@ class GameMode:
         '''What to do when entering this mode'''
         pass
 
-Init(SIZE)
-Game = GameMode()
+class Ball:
+    '''Simple ball class'''
 
+    def __init__(self, filename, pos = (0.0, 0.0), speed = (0.0, 0.0)):
+        '''Create a ball from image'''
+        self.fname = filename
+        self.surface = pygame.image.load(filename)
+        self.rect = self.surface.get_rect()
+        self.speed = speed
+        self.pos = pos
+        self.newpos = pos
+
+    def draw(self, surface):
+        surface.blit(self.surface, self.rect)
+
+    def action(self):
+        '''Proceed some action'''
+        self.pos = self.pos[0]+self.speed[0], self.pos[1]+self.speed[1]
+
+    def logic(self, surface):
+        x,y = self.pos
+        dx, dy = self.speed
+        if x < self.rect.width/2:
+            x = self.rect.width/2
+            dx = -dx
+        elif x > surface.get_width() - self.rect.width/2:
+            x = surface.get_width() - self.rect.width/2
+            dx = -dx
+        if y < self.rect.height/2:
+            y = self.rect.height/2
+            dy = -dy
+        elif y > surface.get_height() - self.rect.height/2:
+            y = surface.get_height() - self.rect.height/2
+            dy = -dy
+        self.pos = x,y
+        self.speed = dx,dy
+        self.rect.center = intn(*self.pos)
+
+class Universe:
+    '''Game universe'''
+
+    def __init__(self, msec, tickevent = pygame.USEREVENT):
+        '''Run a universe with msec tick'''
+        self.msec = msec
+        self.tickevent = tickevent
+
+    def Start(self):
+        '''Start running'''
+        pygame.time.set_timer(self.tickevent, self.msec)
+
+    def Finish(self):
+        '''Shut down an universe'''
+        pygame.time.set_timer(self.tickevent, 0)
+
+class GameWithObjects(GameMode):
+
+    def __init__(self, objects=[]):
+        GameMode.__init__(self)
+        self.objects = objects
+
+    def locate(self, pos):
+        return [obj for obj in self.objects if obj.rect.collidepoint(pos)]
+
+    def Events(self, event):
+        GameMode.Events(self, event)
+        if event.type == Game.tickevent:
+            for obj in self.objects:
+                obj.action()
+
+    def Logic(self, surface):
+        GameMode.Logic(self, surface)
+        for obj in self.objects:
+            obj.logic(surface)
+
+    def Draw(self, surface):
+        GameMode.Draw(self, surface)
+        for obj in self.objects:
+            obj.draw(surface)
+
+
+Init(SIZE)
+Game = Universe(50)
+Run = GameWithObjects([Ball("ball.gif",(1,1),(2,2))])
+
+Game.Start()
+Run.Init()
 again = True
 while again:
     event = pygame.event.wait()
     if event.type == pygame.QUIT:
         again = False
-    Game.Events(event)
-    Game.Logic()
-    Game.Draw(screen)
+    Run.Events(event)
+    Run.Logic(screen)
+    Run.Draw(screen)
     pygame.display.flip()
+Game.Finish()
 pygame.quit()
